498e2158c87756af2b89db7537aeeb56c8400fa3
TalkTakesTime <b3nf@hotmail.com>
2014-03-17 15:44:43 +1300

    Remove ability for .choose to execute commands

--- commands.js
+++ commands.js
@@ -82,7 +82,7 @@ exports.commands = {
 		choices = choices.filter(function(i) {return (toId(i) !== '')});
 		if (choices.length < 2) return this.say(con, room, (room.charAt(0) === ',' ? '': '/pm ' + by + ', ') + '.choose: You must give at least 2 valid choices.');
 		var choice = choices[Math.floor(Math.random()*choices.length)];
-		this.say(con, room, ((this.hasRank(by, '+%@#~') || room.charAt(0) === ',') ? '':'/pm ' + by + ', ') + choice);
+		this.say(con, room, ((this.hasRank(by, '+%@#~') || room.charAt(0) === ',') ? '':'/pm ' + by + ', ') + ((choice.charAt(0) === '/' || choice.charAt(0) === '!') ? ' ':'') + choice);
 	},
 	usage: 'usagestats',
 	usagestats: function(arg, by, room, con) {
