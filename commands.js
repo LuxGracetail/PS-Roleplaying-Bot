902632e5aff09abe458a1c0db2571e04e22a8555
TalkTakesTime <b3nf@hotmail.com>
2014-04-25 16:43:37 +1200

    Add response for .seen on the bot's nick

--- commands.js
+++ commands.js
@@ -253,6 +253,8 @@ exports.commands = {
 		var text = (room.charAt(0) === ',' ? '' : '/pm ' + by + ', ');
 		if (toId(arg) === toId(by)) {
 			text += 'Have you looked in the mirror lately?';
+		} else if (toId(arg) === toId(config.nick)) {
+			text += 'You might be either blind or illiterate. Might want to get that checked out.';
 		} else if (!this.chatData[toId(arg)] || !this.chatData[toId(arg)].lastSeen) {
 			text += 'The user ' + arg.trim() + ' has never been seen.';
 		} else {
