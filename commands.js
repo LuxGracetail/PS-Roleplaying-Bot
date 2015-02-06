1bb40ba54811133eaff1c483498c5208d19c7bd4
Morfent <morfentdoge@gmail.com>
2015-02-06 03:02:43 -0400

    Update usage stats

--- commands.js
+++ commands.js
@@ -419,7 +419,7 @@ exports.commands = {
 		} else {
 			var text = '/pm ' + by + ', ';
 		}
-		text += 'http://www.smogon.com/stats/2014-12/';
+		text += 'http://www.smogon.com/stats/2015-01/';
 		this.say(con, room, text);
 	},
 	seen: function(arg, by, room, con) { // this command is still a bit buggy
