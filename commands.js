993d3824b497d58e50363993e40f25c9df8c12d3
TalkTakesTime <TalkTakesTime@users.noreply.github.com>
2015-03-05 23:39:55 +1300

    Update usage stats link

--- commands.js
+++ commands.js
@@ -421,7 +421,7 @@ exports.commands = {
 		} else {
 			var text = '/pm ' + by + ', ';
 		}
-		text += 'http://www.smogon.com/stats/2015-01/';
+		text += 'http://www.smogon.com/stats/2015-02/';
 		this.say(con, room, text);
 	},
 	seen: function(arg, by, room, con) { // this command is still a bit buggy
