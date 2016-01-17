6a19801624197e20663ed6393c1f694f5c0427a5
LuxGracetail <fiby1998@gmail.com>
2016-01-17 12:33:57 +1100

    Updating getserver.js (Thanks Morf)

--- getserver.js
+++ getserver.js
@@ -1,56 +1,61 @@
-﻿var sys = require('sys');
-var url = require('url');
-var http = require('http');
+﻿'use strict';
 
-if (process.argv[2]) {
-	var args = process.argv.slice(2);
-	var serverUrl = args.join(' ');
-	if (serverUrl.indexOf('://') !== -1) {
-		serverUrl = url.parse(serverUrl).host;
-	}
-	if (serverUrl.slice(-1) === '/') {
-		serverUrl = serverUrl.slice(0, -1);
-	}
+const url = require('url');
+const http = require('http');
 
-	console.log('Getting data for ' + serverUrl + '...');
-	console.log('This may take some time, depending on Showdown\'s speed.');
-
-	var received = false;
-	var requestOptions = {
-		hostname: 'play.pokemonshowdown.com',
-		port: 80,
-		path: '/crossdomain.php?host=' + serverUrl + '&path=',
-		method: 'GET'
-	};
-	var req = http.request(requestOptions, function(res) {
-		res.setEncoding('utf8');
-		res.on('data', function(chunk) {
-			if (received) {
-				return;
-			}
-			received = true;
-			
-			var search = 'var config = ';
-			var index = chunk.indexOf(search);
-			if (index !== -1) {
-				var data = chunk.substr(index);
-				data = data.substr(search.length, data.indexOf(';') - search.length);
-				data = JSON.parse(data);
-				console.log('---------------');
-				console.log('server: ' + data.host);
-				console.log('port: ' + data.port);
-				console.log('serverid: ' + data.id);
-			} else {
-				console.log('ERROR: failed to get data!');
-			}
-		});
-	});
-	
-	req.on('error', function (err) {
-		console.log('ERROR: ' + sys.inspect(err));
-	});
-	
-	req.end();
-} else {
-	console.log('ERROR: no URL specified!');
+if (process.argv.length < 2) {
+	throw new Error('Failed to find data for server: no server URL was given!');
 }
+
+let args = process.argv.slice(2);
+let serverUrl = args.join(' ');
+if (serverUrl.indexOf('://') >= 0) serverUrl = url.parse(serverUrl).host;
+if (serverUrl.endsWith('/')) serverUrl = serverUrl.slice(0, -1);
+
+let received = false;
+let reqOpts = {
+	hostname: 'play.pokemonshowdown.com',
+	port: 80,
+	path: '/crossdomain.php?host=' + serverUrl + '&path=',
+	method: 'GET'
+}
+
+let req = http.request(reqOpts, (res) => {
+	res.setEncoding('utf8');
+
+	res.on('data', (chunk) => {
+		if (received) return false;
+		received = true;
+
+		let search = 'var config = ';
+		let idx = chunk.indexOf(search);
+		if (idx < 0) {
+			throw new Error('Failed to get data for server ' + serverUrl + ': invalid data received: ' + data);
+		}
+
+		let data = chunk.substr(idx);
+		let serverInfo = data.substr(search.length, data.indexOf(';') - search.length);
+
+		try {
+			// No, that's not a typo, we really do need to parse this twice.
+			serverInfo = JSON.parse(JSON.parse(serverInfo));
+		} catch (e) {}
+
+		if (!(serverInfo instanceof Object)) {
+			throw new Error('Failed to get data for server ' + serverUrl + ': invalid data received: ' + serverInfo);
+		}
+
+		console.log(
+			'Server: ' + serverInfo.host + '\n' +
+			'Port: ' + serverInfo.port + '\n' +
+			'Serverid: ' + serverInfo.id + '\n' +
+			'Registered: ' + serverInfo.registered
+		);
+	});
+});
+
+req.on('error', (err) => {
+	throw new Error('Failed to get data for server ' + serverUrl + ': ' + err.message);
+});
+
+req.end();
